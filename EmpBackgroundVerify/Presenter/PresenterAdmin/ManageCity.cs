923f4ae3fbceb80fca64207f1d83b7964c6fce3b
Ujjwal Kumar <68781093+ujjwal509kumar@users.noreply.github.com>
2023-03-24 23:22:04 +0530

    Add files via upload

---
+++ EmpBackgroundVerify/Presenter/PresenterAdmin/ManageCity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Web;
+
+namespace EmpBackgroundVerify.Presenter.PresenterAdmin
+{
+    public class ManageCity
+    {
+        public interface IManageCity
+        {
+            int CityId { get; set; }
+            string CityName { get; set; }
+        }
+
+        public class CityDetails : IManageCity
+        {
+            public int CityId { get; set; }
+            public string CityName { get; set; }
+        }
+    }
+}
