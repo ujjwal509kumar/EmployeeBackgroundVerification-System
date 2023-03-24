0d561585f1392af7446dd5f792d663421f130b53
Ujjwal Kumar <68781093+ujjwal509kumar@users.noreply.github.com>
2023-03-24 23:28:43 +0530

    Add files via upload

---
+++ EmpBackgroundVerify/Common/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+////using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace EmpBackgroundVerify.Common
+{
+    public class ConnectionString
+    {
+        internal static string getConnectionString()
+        {
+
+            return ConfigurationManager.ConnectionStrings["EmpBGverifyConnectionString"].ConnectionString;
+
+        }
+    }
+}
